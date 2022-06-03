90a0aeb92bfef5765915b108a1a9cd5f60d58398
sota70 <sota52350@gmail.com>
2022-06-03 18:57:11 +0900

    fixed a bug that changes every itemstack in chest

--- src/main.py
+++ src/main.py
@@ -7,10 +7,20 @@ dirt_block = ItemStack("dirt", 1)
 wood_block = ItemStack("wood", 2)
 coal = ItemStack("coal", 3)
 temp_chest = Chest(27, "Temporary Chest")
+'''
+これだとインスタンスが一つしか生成されないため
+プロパティを変更すると全て変わってしまう
+ex) ex_block.count = 0をするとtemp_chestのcontents内のdirt_block全てに反映されてしまう
 temp_chest.put_items([
     dirt_block, dirt_block, dirt_block,
     wood_block, wood_block,
     coal
+])
+'''
+temp_chest.put_items([
+    ItemStack("dirt", 1), ItemStack("dirt", 1), ItemStack("dirt", 1),
+    ItemStack("wood", 2), ItemStack("wood", 2),
+    ItemStack("coal", 3)
 ])
 store_chest1 = StoreChest(27, [ 1 ], "Storage1")
 store_chest2 = StoreChest(27, [ 2, 3 ], "Storage2")
